5412951f877ce2c704a744782e81c8c4d71bf5e5
danialrohaizat012-sys <danialrohaizat012@gmail.com>
2026-08-22 02:29:20 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="knc-online-v051-roomfix";
-const ASSETS=["./","./index.html","./manifest.json","./board.png","./brand-logo.png","./splash-logo.png","./icon-192.png","./icon-512.png","./icon-maskable-192.png","./icon-maskable-512.png","./apple-touch-icon.png","./firebase-config.js","./splash-static.jpg"];
+const CACHE="knc-online-v052-functional-splash";
+const ASSETS=["./","./index.html","./manifest.json","./board.png","./brand-logo.png","./splash-logo.png","./icon-192.png","./icon-512.png","./icon-maskable-192.png","./icon-maskable-512.png","./apple-touch-icon.png","./firebase-config.js","./commercial-splash.jpg","./splash-static.jpg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>{
